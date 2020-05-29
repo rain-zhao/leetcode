3dfbd6515fa112fdf0b5cf9e4bb44a7f4a3c7982
rain.zhao <rangeree@foxmail.com>
2020-05-29 10:40:48 +0800

    task 198 dp 解法 压缩空间

--- py/Task198.py
+++ py/Task198.py
@@ -28,3 +28,33 @@ class Solution:
             dp[i] = max(dp[i-2]+num, dp[i-1])
 
         return dp[-1]
+
+    def rob3(self, nums: [int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        # init
+        do = nums[0]
+        nodo = 0
+
+        # loop
+        for num in nums[1:]:
+            do, nodo = nodo + num, max(do, nodo)
+
+        return max(do, nodo)
+
+    def rob4(self, nums: [int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        # init
+        fst = nums[0]
+        sed = max(nums[0], nums[1])
+
+        # loop
+        for num in nums[2:]:
+            fst, sed = sed, max(fst + num, sed)
+
+        return sed
