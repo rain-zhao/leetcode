bbb98353fca930e073a7a5a594c212df1ac12c59
rain.zhao <rangeree@foxmail.com>
2020-07-02 12:05:26 +0800

    task 215 py new solution

--- py/Task215.py
+++ py/Task215.py
@@ -34,6 +34,37 @@ class Solution:
                 pivot = quickselect(nums, i, j)
         return nums[pivot]
 
+    def findKthLargest3(self, nums: List[int], k: int) -> int:
+        # seperate the list by end item
+        def quickSelect(beg: int, end: int) -> int:
+            pivot = beg
+            for i in range(beg, end):
+                if nums[i] < nums[end]:
+                    nums[i], nums[pivot] = nums[pivot], nums[i]
+                    pivot += 1
+            nums[pivot], nums[end] = nums[end], nums[pivot]
+            return pivot
+        target = len(nums) - k
+        beg, end = 0, len(nums)-1
+        while True:
+            pivot = quickSelect(beg, end)
+            if pivot == target:
+                return nums[pivot]
+            elif pivot < target:
+                beg = pivot + 1
+            else:
+                end = pivot - 1
+        return nums[pivot]
+
+    # priority queue
+    def findKthLargest4(self, nums: List[int], k: int) -> int:
+        pq = nums[:k]
+        heapq.heapify(pq)
+        for num in nums[k:]:
+            if pq[0] < num:
+                heapq.heapreplace(pq, num)
+        return pq[0]
+
 
 nums = [3, 2, 1, 5, 6, 4]
 k = 2
