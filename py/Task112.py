52a950a843a79b91d33835057c4e9b65cc62727a
rain.zhao <rangeree@foxmail.com>
2020-07-07 16:05:34 +0800

    no message

--- py/Task112.py
+++ py/Task112.py
@@ -8,10 +8,19 @@ from TreeNode import TreeNode
 
 
 class Solution:
+    # def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+    #     if not root:
+    #         return False
+    #     sum -= root.val
+    #     if not root.left and not root.right:
+    #         return not sum
+    #     return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
             return False
         sum -= root.val
         if not root.left and not root.right:
-            return not sum
+            return sum == 0
+        sum -= root.val
         return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
