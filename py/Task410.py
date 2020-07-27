929579e7c02c4d1d48c1b75e97d73e75e3dd4c5a
rain.zhao <rangeree@foxmail.com>
2020-07-27 11:50:58 +0800

    task 410 new solution:using binary-search

--- py/Task410.py
+++ py/Task410.py
@@ -45,8 +45,52 @@ class Solution:
                     dp[i][j] = min(dp[i][j], max(dp[k][j-1], sub[i]-sub[k]))
         return dp[-1][-1]
 
+    # dp 优化
+    def splitArray3(self, nums: List[int], m: int) -> int:
+        if m == 1:
+            return sum(nums)
+        n = len(nums)
+        sub = [0, ]
+        for num in nums:
+            sub.append(sub[-1]+num)
+        # define
+        dp = [[99999999999] * (m+1) for _ in range(n+1)]
+        # init:
+        for i in range(1, n+1):
+            dp[i][1] = sub[i]
+        # loop
+        for i in range(2, n+1):
+            for j in range(2, min(i, m)+1):
+                for k in range(j-1, i):
+                    dp[i][j] = min(dp[i][j], max(dp[k][j-1], sub[i]-sub[k]))
+        return dp[-1][-1]
+
+    # binary search
+
+    def splitArray4(self, nums: List[int], m: int) -> int:
+        if m == 1:
+            return sum(nums)
+
+        def check(x: int) -> bool:
+            total, cnt = 0, 1
+            for num in nums:
+                total += num
+                if total > x:
+                    cnt += 1
+                    total = num
+            return cnt <= m
+
+        left, right = max(nums), sum(nums)
+        while left <= right:
+            mid = (left + right) >> 1
+            if check(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
+
 
 obj = Solution()
 nums = [7, 2, 5, 10, 8]
 m = 2
-print(obj.splitArray2(nums, m))
+print(obj.splitArray4(nums, m))
