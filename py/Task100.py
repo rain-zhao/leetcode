13fe517917bea6829b77b240a9453cc178bdf39f
rain.zhao <rangeree@foxmail.com>
2020-08-07 10:53:52 +0800

    task 100 py two new solution 1.dfs 2.print tree

--- py/Task100.py
+++ py/Task100.py
@@ -8,6 +8,7 @@
 from TreeNode import TreeNode
 from collections import deque
 
+
 class Solution:
     """
         def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
@@ -17,6 +18,7 @@ class Solution:
                 return not p and not q
     """
 
+    # bfs
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         dq = deque([(p, q)])
         while deque:
@@ -29,3 +31,33 @@ class Solution:
             elif p or q:
                 return False
         return True
+
+    # dfs
+    def isSameTree2(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p:
+            return not q
+        if not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+    # print tree
+    def isSameTree3(self, p: TreeNode, q: TreeNode) -> bool:
+        def printTree(root: TreeNode) -> str:
+            if not root:
+                return 'N'
+            return str(root.val) + '->' + printTree(root.left) + '->' + printTree(root.right)
+        return printTree(p) == printTree(q)
+
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+p.right = TreeNode(3)
+
+q = TreeNode(1)
+q.left = TreeNode(2)
+q.right = TreeNode(3)
+
+obj = Solution()
+print(obj.isSameTree3(p, q))
