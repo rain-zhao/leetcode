ed628ce9d97cb2153c412ec8aa522a99c2afe683
rain.zhao <rangeree@foxmail.com>
2020-08-18 10:48:04 +0800

    task 109 new solution

--- py/Task109.py
+++ py/Task109.py
@@ -15,24 +15,43 @@ from TreeNode import TreeNode
 
 
 class Solution:
+    # fast & slow cursor
     def sortedListToBST(self, head: ListNode) -> TreeNode:
-        def recur(beg:ListNode,end:ListNode)-> TreeNode:
+        def recur(beg: ListNode, end: ListNode) -> TreeNode:
             if beg == end:
                 return None
             p1 = p2 = beg
             while p2.next != end and p2.next.next != end:
-                p1,p2 = p1.next,p2.next.next
+                p1, p2 = p1.next, p2.next.next
             root = TreeNode(p1.val)
-            root.left = recur(beg,p1)
-            root.right = recur(p1.next,end)
+            root.left = recur(beg, p1)
+            root.right = recur(p1.next, end)
             return root
+        return recur(head, None)
 
-        root = recur(head,None)
-        return root
+    # put val into array
+    def sortedListToBST2(self, head: ListNode) -> TreeNode:
+        if not head:
+            return None
+        # put linked list into array
+        array = []
+        pos = head
+        while pos:
+            array.append(pos.val)
+            pos = pos.next
+
+        def transfer(beg: int, end: int) -> TreeNode:
+            if beg > end:
+                return None
+            mid = (beg + end) >> 1
+            root = TreeNode(array[mid])
+            root.left = transfer(beg, mid-1)
+            root.right = transfer(mid+1, end)
+            return root
+        return transfer(0, len(array)-1)
+
 
 head = ListNode(-10)
 head.nextItm(-3).nextItm(0).nextItm(5).nextItm(9)
 so = Solution()
-so.sortedListToBST(head)
-
-
+so.sortedListToBST3(head)
