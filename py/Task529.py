18d379034a7a6d26d0041a2375fb424705da1c3e
rain.zhao <rangeree@foxmail.com>
2020-08-20 19:16:11 +0800

    Task 529 py

---
+++ py/Task529.py
@@ -0,0 +1,83 @@
+from typing import List
+
+
+class Solution:
+    # dfs
+    def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            return board
+        m, n = len(board), len(board[0])
+        direct = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1]
+        ]
+
+        def dfs(x: int, y: int):
+            res = 0
+            queue = []
+            for dx, dy in direct:
+                xx, yy = x+dx, y+dy
+                if not 0 <= xx < m or not 0 <= yy < n:
+                    continue
+                if board[xx][yy] == 'M':
+                    res += 1
+                    continue
+                if board[xx][yy] != 'E':
+                    continue
+                queue.append((xx, yy))
+
+            if res == 0:
+                board[x][y] = 'B'
+                for xx, yy in queue:
+                    dfs(xx, yy)
+            else:
+                board[x][y] = str(res)
+        dfs(click[0], click[1])
+        return board
+
+    # dfs 简化代码
+    def updateBoard2(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
+        if board[click[0]][click[1]] == 'M':
+            board[click[0]][click[1]] = 'X'
+            return board
+        m, n = len(board), len(board[0])
+        direct = [
+            [-1, -1], [-1, 0], [-1, 1],
+            [0, -1], [0, 1],
+            [1, -1], [1, 0], [1, 1]
+        ]
+
+        def dfs(x: int, y: int):
+            cnt = 0
+            for dx, dy in direct:
+                xx, yy = x+dx, y+dy
+                if 0 <= xx < m and 0 <= yy < n and board[xx][yy] == 'M':
+                    cnt += 1
+
+            if cnt > 0:
+                board[x][y] = str(cnt)
+            else:
+                board[x][y] = 'B'
+                for dx, dy in direct:
+                    xx, yy = x+dx, y+dy
+                    if 0 <= xx < m and 0 <= yy < n and board[xx][yy] == 'E':
+                        dfs(x+dx, y+dy)
+
+        dfs(*click)
+        return board
+
+
+# board = [
+#     ["B", "1", "E", "1", "B"],
+#     ["B", "1", "M", "1", "B"],
+#     ["B", "1", "1", "1", "B"],
+#     ["B", "B", "B", "B", "B"]
+# ]
+# click = [1, 2]
+board = [["E", "E", "E", "E", "E"], ["E", "E", "M", "E", "E"],
+         ["E", "E", "E", "E", "E"], ["E", "E", "E", "E", "E"]]
+click = [3, 0]
+obj = Solution()
+print(obj.updateBoard2(board, click))
