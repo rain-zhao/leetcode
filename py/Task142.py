77039c8212635ba004864ee9050237af248b4457
rain.zhao <rangeree@foxmail.com>
2020-10-10 10:21:53 +0800

    task 142 py

--- py/Task142.py
+++ py/Task142.py
@@ -8,17 +8,27 @@ class ListNode:
 class Solution:
     # floyd
     def detectCycle(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return None
         p1 = p2 = head
-        while p1 and p1.next:
-            p1 = p1.next.next
-            p2 = p2.next
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
             if p1 == p2:
                 break
-        if p1 != p2:
+        if not p2 or not p2.next:
             return None
         p1 = head
         while p1 != p2:
-            p1, p2 = p1.next, p2.next
+            p1 = p1.next
+            p2 = p2.next
         return p1
+
+    # hash
+    def detectCycle2(self, head: ListNode) -> ListNode:
+        hash = set()
+        p = head
+        while p:
+            if p in head:
+                return p
+            hash.add(p)
+            p = p.next
+        return None
