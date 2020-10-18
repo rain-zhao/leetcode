7a5117ee12f34e697b3d66970faa92a759098e41
rain.zhao <rangeree@foxmail.com>
2020-10-18 16:55:34 +0800

    task 19 py

---
+++ py/Task19.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(-1)
+        dummy.next = head
+        p1 = dummy
+        for _ in range(n):
+            p1 = p1.next
+        p2 = dummy
+        while p1.next:
+            p1 = p1.next
+            p2 = p2.next
+        p2.next = p2.next.next
+        return dummy.next
