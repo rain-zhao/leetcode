722ccd889d541b15665eaaf64b2343ac4867e489
rain.zhao <rangeree@foxmail.com>
2020-10-20 16:53:55 +0800

    task 143 optimize code

--- py/Task143.py
+++ py/Task143.py
@@ -8,9 +8,6 @@ from ListNode import ListNode
 
 class Solution:
     def reorderList(self, head: ListNode) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
         if not head:
             return
         arr = []
@@ -20,39 +17,35 @@ class Solution:
             head = head.next
         # reorder
         size = len(arr)
-        dummy = ListNode(-1)
-        pre = dummy
-        for i, j in zip(arr[:size//2], arr[-1:(size-1)//2:-1]):
-            pre.next, i.next, pre = i, j, j
+        pre = dummy = ListNode(-1)
+        for i in range(size >> 1):
+            pre.next, arr[i].next, pre = arr[i], arr[-i-1], arr[-i-1]
+        # odd
         if size & 1:
-            pre.next, pre = arr[size//2], arr[size//2]
+            pre.next, pre = arr[size >> 1], arr[size >> 1]
         pre.next = None
 
     def reorderList2(self, head: ListNode) -> None:
-        if not head or not head.next:
+        if not head:
             return
-        # find mid pos
+        # find mid
         p1 = p2 = head
         while p2.next and p2.next.next:
             p1, p2 = p1.next, p2.next.next
-        # p1 is mid pos
-        # reverse link list after p1
-        pre, cur, p1.next = None, p1.next, None
-        while cur:
-            cur.next, cur, pre = pre, cur.next, cur
-        # pre is the reverse list's head
+        # p1 is mid
+        # reverse the second sequence
+        pre, p, p1.next = None, p1.next, None
+        while p:
+            p.next, pre, p = pre, p, p.next
         dummy = ListNode(-1)
-        p1, p2, pre = head, pre, dummy
+        pre, p1, p2 = dummy, head, pre
         while p2:
-            pre.next, p1.next, p1, p2, pre = p1, p2, p1.next, p2.next, p2
-
+            pre.next, p1.next, pre, p1, p2 = p1, p2, p2, p1.next, p2.next
         if p1:
             pre.next = p1
 
-        return dummy.next
-
 
 head = ListNode(1)
-head.nextItm(2).nextItm(3).nextItm(4)
+head.nextItm(2).nextItm(3).nextItm(4).nextItm(5)
 so = Solution()
-so.reorderList2(head)
+so.reorderList3(head)
