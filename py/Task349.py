5cbf651bf106c3edac2a20f81da582799653fb45
rain.zhao <rangeree@foxmail.com>
2020-11-02 17:14:11 +0800

    task 349 py

--- py/Task349.py
+++ py/Task349.py
@@ -4,10 +4,18 @@ from collections import Counter
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return set(nums1) & set(nums2) 
+        return set(nums1) & set(nums2)
+
+    def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1, set2 = set(nums1), set(nums2)
+        res = []
+        for num in set1:
+            if num in set2:
+                res.append(num)
+        return res
 
 
 nums1 = [1, 2, 2, 1]
 nums2 = [2, 2]
 so = Solution()
-print(so.intersection(nums1, nums2))
+print(so.intersection2(nums1, nums2))
