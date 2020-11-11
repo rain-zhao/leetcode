e6bb6b79d7da5a10c2886270c1245dbc811a1ca0
rain.zhao <rangeree@foxmail.com>
2020-11-11 19:47:28 +0800

    task 31 py optimize code

--- py/Task31.py
+++ py/Task31.py
@@ -32,7 +32,25 @@ class Solution:
             i += 1
             j -= 1
 
+    def nextPermutation2(self, nums: List[int]) -> None:
+        n = len(nums)
+        # find last asc idx
+        i = n - 1
+        while i > 0 and nums[i] <= nums[i-1]:
+            i = i-1
 
-nums = [1, 2, 3]
+        # reverse from i to tail
+        for j in range(i, (n+i) >> 1):
+            nums[j], nums[i-j-1] = nums[i-j-1], nums[j]
+
+        # find first j gt i and swap i-1 & j
+        j = i
+        while j < n-1 and nums[j] <= nums[i-1]:
+            j += 1
+        nums[j], nums[i-1] = nums[i-1], nums[j]
+
+
+nums = [4, 3, 2, 1]
 obj = Solution()
-print(obj.nextPermutation(nums))
+obj.nextPermutation2(nums)
+print(nums)
