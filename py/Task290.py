dc95370d5fd4ab5b6c1bb79ccee14f2731389c39
rain.zhao <rangeree@foxmail.com>
2020-12-16 12:58:37 +0800

    task 290 py

--- py/Task290.py
+++ py/Task290.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class Solution:
     def wordPattern(self, pattern: str, str: str) -> bool:
         str = str.split()
@@ -13,8 +16,20 @@ class Solution:
                 return False
         return True
 
+    def wordPattern2(self, pattern: str, str: str) -> bool:
+        arr = str.split()
+        if len(pattern) != len(arr):
+            return False
+        mp, s = {}, set()
+        for p, word in zip(pattern, arr):
+            if p in mp and mp[p] != word or p not in mp and word in s:
+                return False
+            mp[p] = word
+            s.add(word)
+        return True
+
 
 pattern = "abba"
 str = "dog cat cat dog"
 so = Solution()
-print(so.wordPattern(pattern, str))
+print(so.wordPattern2(pattern, str))
