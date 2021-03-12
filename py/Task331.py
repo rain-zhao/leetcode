5984d1322143b0aef744b53a6fabedee34977e7c
rain.zhao <rangeree@foxmail.com>
2021-03-12 16:43:48 +0800

    task 331 py

--- py/Task331.py
+++ py/Task331.py
@@ -1,4 +1,5 @@
 class Solution:
+    # recursion using stack
     def isValidSerialization(self, preorder: str) -> bool:
         q = preorder.split(',')
         q.reverse()
@@ -13,7 +14,37 @@ class Solution:
 
         return recursion() and not q
 
+    # recursion(dfs) using array iteration
+    def isValidSerialization2(self, preorder: str) -> bool:
+        nodes = preorder.split(',')
+        n = len(nodes)
+        i = 0
 
-preorder = "9,#,#,1"
+        def dfs():
+            nonlocal i
+            if i == n:
+                return False
+            node = nodes[i]
+            i += 1
+            if node == '#':
+                return True
+            else:
+                return dfs() and dfs()
+
+        return dfs() and i == n
+
+    # iteratrion
+    def isValidSerialization3(self, preorder: str) -> bool:
+        slots = 1
+        for node in preorder.split(','):
+            if not slots:
+                return False
+            slots -= 1
+            if node != '#':
+                slots += 2
+        return not slots
+
+
+preorder = "1,#"
 so = Solution()
-print(so.isValidSerialization(preorder))
+print(so.isValidSerialization3(preorder))
