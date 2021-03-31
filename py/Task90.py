36218ce7546e5a2e9c02b7b27d1f15291a6910b5
rain.zhao <rangeree@foxmail.com>
2021-03-31 12:33:32 +0800

    task 90 py new solution using iteration

--- py/Task90.py
+++ py/Task90.py
@@ -1,5 +1,9 @@
+from typing import List
+from collections import Counter
+
+
 class Solution:
-    def subsetsWithDup(self, nums: [int]) -> [[int]]:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         if not nums:
             return []
         cnts = {}
@@ -19,7 +23,35 @@ class Solution:
         dfs([], items, 0)
         return res
 
+    # iteration
+    def subsetsWithDup2(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = [[]]
+        i, n = 0, len(nums)
+        while i < n:
+            num = nums[i]
+            count = 1
+            i += 1
+            while i < n and nums[i] == num:
+                count += 1
+                i += 1
+            for candidate in res[:]:
+                for cnt in range(1, count+1):
+                    res.append(candidate + [num]*cnt)
+        return res
+
+    # iteration + using counter
+    def subsetsWithDup3(self, nums: List[int]) -> List[List[int]]:
+        counter = Counter(nums)
+        res = [[]]
+        for key in counter:
+            count = counter[key]
+            for candidate in res[:]:
+                for i in range(1, count+1):
+                    res.append(candidate + [key] * i)
+        return res
+
 
 nums = [1, 2, 2]
 so = Solution()
-print(so.subsetsWithDup(nums))
+print(so.subsetsWithDup3(nums))
