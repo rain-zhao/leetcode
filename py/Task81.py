7c69d7405c917e4b5d274822df534fc41e481f82
rain.zhao <rangeree@foxmail.com>
2021-04-07 17:31:08 +0800

    task 81 py ,optimize code , binary search

--- py/Task81.py
+++ py/Task81.py
@@ -34,3 +34,39 @@ class Solution:
             else:
                 right = mid - 1
         return False
+
+    # 2021-04-07
+    def search2(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        if n <= 8:
+            return target in nums
+
+        # 1 find min idx,题目已经保证数组已经经过旋转
+        left, right = 0, n-2
+        shift = -1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] > nums[mid+1]:
+                shift = mid + 1
+                break
+            elif nums[mid] < nums[left]:
+                right = mid - 1
+            elif nums[mid] > nums[left]:
+                left = mid + 1
+            else:
+                left += 1
+
+        if nums[shift] > target or nums[shift-1] < target:
+            return False
+
+        # binary search
+        left, right = shift, n - 1 + shift
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid % n] == target:
+                return True
+            elif nums[mid % n] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
