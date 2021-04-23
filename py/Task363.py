317b6e1370256b9238772aa8422e119786b7c4c4
rain.zhao <rangeree@foxmail.com>
2021-04-23 14:30:28 +0800

    task 363 py using dp

--- py/Task363.py
+++ py/Task363.py
@@ -22,8 +22,24 @@ class Solution:
                     sortedList.add(s)
         return res
 
+    # dp
+    def maxSumSubmatrix2(self, matrix: List[List[int]], k: int) -> int:
+        m, n = len(matrix), len(matrix[0])
+        res = -10**5
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp = [[0] * (n+1) for _ in range(m+1)]
+                dp[i][j] = matrix[i-1][j-1]
+                for p in range(i, m+1):
+                    for q in range(j, n+1):
+                        dp[p][q] = dp[p][q-1] + dp[p-1][q] - \
+                            dp[p-1][q-1] + matrix[p-1][q-1]
+                        if dp[p][q] <= k and dp[p][q] > res:
+                            res = dp[p][q]
+        return res
 
-matrix = [[2,2,-1]]
+
+matrix = [[2, 2, -1]]
 k = 3
 obj = Solution()
-print(obj.maxSumSubmatrix(matrix, k))
+print(obj.maxSumSubmatrix2(matrix, k))
