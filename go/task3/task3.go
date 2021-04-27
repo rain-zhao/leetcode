b7c47d5ffaf9bfada9028d82a28013974c1694f0
rain.zhao <rangeree@foxmail.com>
2021-04-27 22:30:56 +0800

    task3 go

---
+++ go/task3/task3.go
@@ -0,0 +1,27 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	appear := make(map[rune]int)
+	left := -1
+	res := 0
+	for right, c := range s {
+		if p, ok := appear[c]; ok {
+			if left < p {
+				left = p
+			}
+		}
+		appear[c] = right
+		if l := right - left; l > res {
+			res = l
+		}
+	}
+	return res
+}
+
+func main() {
+	s := "abcabcbb"
+	println(lengthOfLongestSubstring(s))
+}
