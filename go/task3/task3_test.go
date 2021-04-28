e7b859645e490a82b6480c97a88ad5d9cce6ce74
rain.zhao <rangeree@foxmail.com>
2021-04-28 19:00:09 +0800

    change test file

--- go/task3/task3_test.go
+++ go/task3/task3_test.go
@@ -1,8 +1,25 @@
 package task3
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestTask3(t *testing.T) {
-	s := "abcabcbb"
-	t.Log(lengthOfLongestSubstring(s))
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"test1", args{"abcabcbb"}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstring(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstring() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
