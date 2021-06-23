d9ea369ac7c400486e68b953cf56e2deef56668d
zhaozhenrong <zhaozhenrong@bytedance.com>
2021-06-23 22:01:46 +0800

    task offer15 golang

---
+++ go/offer15/offer15_test.go
@@ -0,0 +1,24 @@
+package offer15
+
+import "testing"
+
+func Test_hammingWeight(t *testing.T) {
+	type args struct {
+		num uint32
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"test case 1", args{9}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hammingWeight(tt.args.num); got != tt.want {
+				t.Errorf("hammingWeight() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
