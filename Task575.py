e5734451fb722cf6ffc8da9ca8560f33523664c4
zhaozhenrong <zhaozhenrong@bytedance.com>
2021-11-01 13:41:31 +0800

    optimize code task 575

--- Task575.py
+++ Task575.py
@@ -4,7 +4,7 @@ from typing import Counter, List
 class Solution:
     # using heap
     def distributeCandies(self, candyType: List[int]) -> int:
-        return min(len(Counter(candyType).keys()), len(candyType) >> 1)
+        return min(len(set(candyType)), len(candyType) >> 1)
 
 
 candies = [1, 1, 2, 2, 3, 3]
