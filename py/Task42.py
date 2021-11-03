507c2461345300db65a67f4c3c7e5da7c99e89c7
zhaozhenrong <zhaozhenrong@bytedance.com>
2021-11-03 17:01:55 +0800

    task 42 optimize code

--- py/Task42.py
+++ py/Task42.py
@@ -4,74 +4,53 @@ from typing import List
 class Solution:
     # dp
     def trap(self, height: List[int]) -> int:
-        l = len(height)
-        if l < 3:
-            return 0
-
-        # define and init
-        lefts = [0] * (l)
-        rights = [0] * (l)
-
-        # loop lefts
-        maxLeft = 0
-        for i in range(l):
-            maxLeft = max(maxLeft, height[i])
-            lefts[i] = maxLeft
-
-        # loop right:
-        maxRight = 0
-        for i in range(l - 1, -1, -1):
-            maxRight = max(maxRight, height[i])
-            rights[i] = maxRight
+        n = len(height)
+        lefts, rights = height[:], height[:]
+        for i in range(1, n):
+            lefts[i] = max(lefts[i], lefts[i-1])
+            rights[n-1-i] = max(rights[n-1-i], rights[n-i])
 
         res = 0
-        # calculate
-        for i in range(l):
+        for i in range(1, n-1):
             res += min(lefts[i], rights[i]) - height[i]
-
         return res
 
-    # stack
-    def trap2(self, height: List[int]) -> int:
-        l = len(height)
-        if l < 3:
+    # monotone stack
+    def trap4(self, height: List[int]) -> int:
+        n = len(height)
+        if n < 3:
             return 0
+
+        stack = [0]
         res = 0
-        stack = []
-        for i, h in enumerate(height):
-            while stack and height[stack[-1]] <= h:
-                bottom_idx = stack.pop()
+        for i in range(1, n):
+            while height[stack[-1]] <= height[i]:
+                ii = stack.pop()
                 if not stack:
                     break
-                delta = min(height[stack[-1]], h) - height[bottom_idx]
-                distance = i - stack[-1] - 1
-                res += delta * distance
+                x = i - stack[-1] - 1
+                y = min(height[stack[-1]], height[i]) - height[ii]
+                res += x * y
             stack.append(i)
         return res
 
     # double cursor
     def trap3(self, height: List[int]) -> int:
-        l = len(height)
-        if l < 3:
-            return 0
+        left, right = 0, len(height)-1
+        maxl, maxr = height[left], height[right]
         res = 0
-
-        left, right = 0, l-1
-        maxLeft = maxRight = 0
-
         while left < right:
             if height[left] < height[right]:
-                maxLeft = max(maxLeft, height[left])
-                res += maxLeft - height[left]
+                maxl = max(maxl, height[left])
+                res += maxl - height[left]
                 left += 1
             else:
-                maxRight = max(maxRight, height[right])
-                res += maxRight - height[right]
+                maxr = max(maxr, height[right])
+                res += maxr - height[right]
                 right -= 1
-
         return res
 
 
 obj = Solution()
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-print(obj.trap3(height))
+print(obj.trap4(height))
